8f211c57445d2086ed62b8c5c0aa38f1cd023255
George Grigorev <deezchannel@gmail.com>
2016-09-20 14:17:37 +0300

    Initial commit

---
+++ CalculatorDemo/CalcViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CalculatorDemo
+//
+//  Created by George on 19.09.16.
+//  Copyright © 2016 George Grigorev. All rights reserved.
+//
+
+import UIKit
+
+class CalcViewController: UIViewController {
+
+    
+
+
+}
+
